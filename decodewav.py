915f6a5635b559d30a2b3413bbffa84dfc309bcb
Justus Schneider <justus@linux.fritz.box>
2025-11-25 01:32:32 +0100

    Made some small improvements, decoding still not working

--- decodewav.py
+++ decodewav.py
@@ -6,55 +6,102 @@ from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import scipy.fftpack
 import scipy
+from scipy import fft
 from scipy import signal
 import os
 from pydub import AudioSegment
 
-parser = argparse.ArgumentParser(description="Decrypt wav file")	
-parser.add_argument("-n", action="store", dest='name', type=str, default="out.wav",
-    help="Manually configure filename")
-parser.add_argument("-in", action="store", dest='intype', type=str, default="")
+parser = argparse.ArgumentParser(description="Decrypt wav file")
+parser.add_argument(
+    "-n",
+    action="store",
+    dest="name",
+    type=str,
+    default="out.wav",
+    help="Manually configure filename",
+)
+parser.add_argument("-in", action="store", dest="intype", type=str, default="")
 args = parser.parse_args()
 
-rate = 48000    # samples per second, every second 44100 samples are used, for 100ms --> 44100/(1000/100)
+rate = 48000  # samples per second, every second 44100 samples are used, for 100ms --> 44100/(1000/100)
 filename = args.name
 
-T = 1         # sample duration for each bit (seconds), can be changed using the ms down below
-f1 = 22000.0   # sound frequency (Hz) for 0 bit
-f2 = 23000.0   # sound frequency (Hz) for 1 bit
-start_sequence = 20000.0   # start frequencies
-stop_sequence = 20000.0    # stop frequency
+T = 1  # sample duration for each bit (seconds), can be changed using the ms down below
+f1 = 22000.0  # sound frequency (Hz) for 0 bit
+f2 = 23000.0  # sound frequency (Hz) for 1 bit
+start_sequence = 20000.0  # start frequencies
+stop_sequence = 20000.0  # stop frequency
 
 
-ms = 5; #milliseconds between each bit
-samples = rate//(1000//ms)
+ms = 5  # milliseconds between each bit
+samples = rate // (1000 // ms)
 
-if f1 > rate//2 or f2 > rate//2:
-	print("Error, maximum frequency exceeds " + str(rate/2))
-	exit()
+if f1 > rate // 2 or f2 > rate // 2:
+    print("Error, maximum frequency exceeds " + str(rate / 2))
+    exit()
 
 rate, data = wavfile.read(filename)
 # rate=48000, data.shape=(46447, 2) ~ almost 1s of stereo signal
 
 if args.intype == "audio":
-    data = data[:,0]
+    data = data[:, 0]
 
 print(rate)
 
 header_len_data = data[:5800]
 
-f, t, Sxx = signal.spectrogram(data, rate, mode="magnitude", nperseg=270)  # t starts at 1 ms as index 0
+f, t, Sxx = signal.spectrogram(data, rate)  # , mode="magnitude", nperseg=270
+# )  # t starts at 1 ms as index 0
+
+print(t.shape, f.shape, Sxx.shape)
 
 plt.pcolormesh(t, f, Sxx)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+plt.ylabel("Frequency [Hz]")
+plt.xlabel("Time [sec]")
 plt.savefig("data.png")
 plt.show()
 
+freqs = fft.fftshift(fft.fftfreq(240, d=1/rate))[240//2:240]
+
+print(freqs)
 """
-for freq in header_len_data_sxx:
-    if (freq - f1 > freq - f2):
-        bit = 0
-    else:
-        bit = 1
+for sample in Sxx:
+    if(sample.size > 0):
+        if (sample[np.where(freqs == f1)] > sample[np.where(freqs == f2)]):
+            bit = 0
+        elif (sample[np.where(freqs == f1)] < sample[np.where(freqs == f2)]):
+            bit = 1
+
+        print(bit)
 """
+
+def getFreq(start):
+    end = start + 10
+    sr, data = wavfile.read(filename)
+    sp = int(sr * start / 1000)
+    ep = int(sr * end / 1000)
+    l = 10 / 1000
+    c = 0
+    for i in range(sp, ep):
+        if i + 1 >= len(data):
+            break
+        if data[i] < 0 and data[i + 1] > 0:
+            c += 1
+    return int(c / l)
+
+def get_duration_pydub(file_path):
+   audio_file = AudioSegment.from_file(file_path)
+   duration = audio_file.duration_seconds
+   print(duration * 1000)
+   return duration
+
+#bits = ""
+
+#for i in range(0, int(get_duration_pydub(filename) * 1000), ms):
+#    print(getFreq(i))
+#    if getFreq(i) == f1 - 1000:
+#        bits = bits + "0"
+#    elif getFreq(i) == f2 - 1000:
+#        bits = bits + "1"
+
+#print(bits)
--- generatewav.py
+++ generatewav.py
@@ -4,107 +4,162 @@ import numpy as np
 import argparse
 import header as hd
 from pydub import AudioSegment
+from bitstring import BitArray
+from alive_progress import alive_bar
+
 
 def combine(input_file, inject_file, output_file):
-	sound1 = AudioSegment.from_file(input_file)
-	inject_raw = AudioSegment.from_file(inject_file)
-	sound2 = inject_raw - 5
-	combined = sound1.overlay(sound2)
-	combined.export(output_file, format='wav')
+    sound1 = AudioSegment.from_file(input_file)
+    inject_raw = AudioSegment.from_file(inject_file)
+    sound2 = inject_raw - 5
+    combined = sound1.overlay(sound2)
+    combined.export(output_file, format="wav")
+
 
 parser = argparse.ArgumentParser(description="Encrypt wav file")
-parser.add_argument("-r", action="store", dest='rate', type=int, default=48000,
-	help="Set sampling rate")	# samples per second, every second 44100 samples are used, for 100ms --> 44100/(1000/100)
-parser.add_argument("-d", action="store", dest="data", type=str,
-    help="Input data to transform")
-parser.add_argument("-f1", action="store", dest="f1", type=float, default=22000.0,
-    help="Input low bit frequency")
-parser.add_argument("-f2", action="store", dest="f2", type=float, default=23000.0,
-    help="Input high bit frequency")
-parser.add_argument("-head", action="store", dest="header", type=str, default="short", 
-	help="Choose which header to use")
-parser.add_argument("-t", action="store", dest="type", type=str, default="txt",
-    help="Define the filetype to put in the custom header")
+parser.add_argument(
+    "-r", action="store", dest="rate", type=int, default=48000, help="Set sampling rate"
+)  # samples per second, every second 44100 samples are used, for 100ms --> 44100/(1000/100)
+parser.add_argument(
+    "-d", action="store", dest="data", type=str, help="Input data to transform"
+)
+parser.add_argument(
+    "-f1",
+    action="store",
+    dest="f1",
+    type=float,
+    default=22000.0,
+    help="Input low bit frequency",
+)
+parser.add_argument(
+    "-f2",
+    action="store",
+    dest="f2",
+    type=float,
+    default=23000.0,
+    help="Input high bit frequency",
+)
+parser.add_argument(
+    "-head",
+    action="store",
+    dest="header",
+    type=str,
+    default="short",
+    help="Choose which header to use",
+)
+parser.add_argument(
+    "-t",
+    action="store",
+    dest="type",
+    type=str,
+    default="txt",
+    help="Define the filetype to put in the custom header",
+)
 parser.add_argument("-in", action="store", dest="input", type=str)
 parser.add_argument("-out", action="store", dest="output", type=str, default="out.wav")
 args = parser.parse_args()
 
 head = args.header
 rate = args.rate
-file_type = "txt" #args.type
+file_type = args.type
 
-T = 1         				# sample duration for each bit (seconds), can be changed using the ms down below
-f1 = args.f1   				# sound frequency (Hz) for 0 bit
-f2 = args.f2   				# sound frequency (Hz) for 1 bit
+T = 1  # sample duration for each bit (seconds), can be changed using the ms down below
+f1 = args.f1  # sound frequency (Hz) for 0 bit
+f2 = args.f2  # sound frequency (Hz) for 1 bit
 
 
-ms = 5; #milliseconds between each bit
-samples = rate//(1000//ms)
+ms = 5  # milliseconds between each bit
+samples = rate // (1000 // ms)
 
-if f1 > rate//2 or f2 > rate//2:
-	print("Error, maximum frequency exceeds " + str(rate/2))
-	exit()
+if f1 > rate // 2 or f2 > rate // 2:
+    print("Error, maximum frequency exceeds " + str(rate / 2))
+    exit()
 
 if args.data:
-	s = args.data
+    s = args.data
 else:
-	s = input("Enter your data: ")
+    s = input("Enter your data: ")
+    print("Input Stream: " + ("".join(map(bin, bytearray(s, "utf-8")))) + "\n")
+    binout = (("".join(map(bin, bytearray(s, "utf-8")))).replace("b", "")).replace(" ", "")
 
 x = []
 l = []
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 if args.data:
-	if file_type == "txt":
-		s = open(s, "r")
-		string_s = ""
-		for line in s:
-			string_s = string_s + line
-		s = string_s
+    if file_type == "txt":
+        s = open(s, "r")
+        string_s = ""
+        for line in s:
+            string_s = string_s + line
+        s = string_s
+        print("Input Stream: " + ("".join(map(bin, bytearray(s, "utf-8")))) + "\n")
+        binout = (("".join(map(bin, bytearray(s, "utf-8")))).replace("b", "")).replace(" ", "")
+    elif file_type == "audio":
+        binout = BitArray(bytes=open(s, "rb").read())
 
-
-print("Input Stream: " + (''.join(map(bin,bytearray(s, 'utf-8')))) + "\n")
-binout = ((''.join(map(bin,bytearray(s, 'utf-8')))).replace("b", "")).replace(" ", "")
 print("Binout: %s \n" % (binout))
 file_len = len(binout)
 print(file_len)
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # start sequence with start_sequence frequency
 
 if head == "standard":
-	header_bits = len(bin(ms)) + len(bin(file_len))
-	print(header_bits)
-	hd.writeheaderdata_padded(T, 48000, x, 22000.0, 23000.0, 48000//(1000//ms), header_bits)
-	hd.standard(T, rate, x, f1, f2, samples, ms, file_len)						# set up header
+    header_bits = len(bin(ms)) + len(bin(file_len))
+    print(header_bits)
+    hd.writeheaderdata_padded(
+        T, 48000, x, 22000.0, 23000.0, 48000 // (1000 // ms), header_bits
+    )
+    hd.standard(T, rate, x, f1, f2, samples, ms, file_len)  # set up header
 elif head == "short":
-	header_bits = len(bin(file_len)) - 1
-	print(header_bits)
-	hd.writeheaderdata_padded(T, 48000, x, 22000.0, 23000.0, 48000//(1000//ms), header_bits)
-	hd.short(T, rate, x, f1, f2, ms, file_len)							# set up header
+    header_bits = len(bin(file_len)) - 1
+    print(header_bits)
+    hd.writeheaderdata_padded(
+        T, 48000, x, 22000.0, 23000.0, 48000 // (1000 // ms), header_bits
+    )
+    hd.short(T, rate, x, f1, f2, ms, file_len)  # set up header
 elif head == "custom":
-	header_bits = len(bin(int(f1//1000))) + len(bin(int(f2//1000))) + len(bin(rate//1000)) + len(bin(5)) + len(bin(file_len)) + 8 * 3 - 9
-	print(header_bits)
-	hd.writeheaderdata_padded(T, 48000, x, 22000.0, 23000.0, 48000//(1000//ms), header_bits)
-	hd.custom(T, rate, x, f1, f2, file_len, ms, file_type)	# set up header
+    header_bits = (
+        len(bin(int(f1 // 1000)))
+        + len(bin(int(f2 // 1000)))
+        + len(bin(rate // 1000))
+        + len(bin(5))
+        + len(bin(file_len))
+        + 8 * 3
+        - 9
+    )
+    print(header_bits)
+    hd.writeheaderdata_padded(
+        T, 48000, x, 22000.0, 23000.0, 48000 // (1000 // ms), header_bits
+    )
+    hd.custom(T, rate, x, f1, f2, file_len, ms, file_type)  # set up header
 
-# transform bitstream to corresponding frequency 
-for i in binout:
-	t = np.linspace(0, T, T*rate, endpoint=False)
-	if i == '0':
-		x.append(np.sin(2*np.pi * f1 * t[:samples]))
-	if i == '1':
-		x.append(np.sin(2*np.pi * f2 * t[:samples]))
+# transform bitstream to corresponding frequency
+#progress_counter = 0
+with alive_bar(len(binout), force_tty=True) as bar:
+    for i in binout:
+        #print("Processed ", progress_counter, " samples of total ", file_len)
+        t = np.linspace(0, T, T * rate, endpoint=False)
+        if i == "0":
+            x.append(np.sin(2 * np.pi * f1 * t[:samples]))
+        if i == "1":
+            x.append(np.sin(2 * np.pi * f2 * t[:samples]))
+        #progress_counter += 1
+        bar()
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-for i in x:
-	for j in i:
-		l.append(j);
+with alive_bar(len(x) * 240, force_tty=True) as bar:
+    for i in x:
+        for j in i:
+            l.append(j)
+            bar()
 
 l = np.array(l)
 
 if args.input:
-	wavio.write(args.output, l, rate, sampwidth=2)
-	combine(args.input, args.output, args.output)
+    wavio.write(args.output, l, rate, sampwidth=2)
+    combine(args.input, args.output, args.output)
 else:
-	wavio.write(args.output, l, rate, sampwidth=2)
+    wavio.write(args.output, l, rate, sampwidth=2)
+
